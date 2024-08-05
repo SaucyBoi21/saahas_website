fd05ba6492fddaadfd33c7554de6674b20b746db
Saahas Swaroop <72944967+SaucyBoi21@users.noreply.github.com>
2024-08-05 16:26:04 -0400

    added better routing

--- server/server.ts
+++ server/server.ts
@@ -1,8 +1,11 @@
-const routes = require("./routes/routes")
+const spotify = require("./routes/spotify")
+const github = require("./routes/github")
 const express = require("express")
 const app = express()
 
 const port = 3000
 
 app.use(express.json())
-app.use("/api", routes)
+
+app.use("/api/spotify", spotify)
+app.use("/api/github", github)
