689a1d7a0613f0ed55ac8d37925a29baedaed181
Saahas Swaroop <saahas.swaroop@googlemail.com>
2026-01-18 19:47:48 -0500

    more config stuff

--- app/react-router.config.ts
+++ app/react-router.config.ts
@@ -1,8 +1,15 @@
 import type { Config } from "@react-router/dev/config";
 
 export default {
-  // Config options...
-  // Server-side render by default, to enable SPA mode set this to `false`
-  ssr: true,
-  basename: "/saahas_website/"
+  appDirectory: "app",
+
+  // We are deploying to GitHub Pages (static hosting), so disable SSR.
+  ssr: false,
+
+  // Pre-render routes to static HTML files (GitHub Pages needs index.html).
+  prerender: true,
+
+  // Your site lives at /saahas_website on GitHub Pages.
+  basename: "/saahas_website",
 } satisfies Config;
+
